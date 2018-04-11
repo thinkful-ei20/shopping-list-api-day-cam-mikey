574de15001d6d4884bed7b52dedcddf8a33209ab
Cam Prier <CSPrier@gmail.com>
2018-04-11 12:13:28 -0700

    Added createIem method in api.js

--- scripts/api.js
+++ scripts/api.js
@@ -6,8 +6,16 @@ const api = (function() {
   function getItems(callback) {
     callback('api module works');
   }
-  
+
+  // This method will POST request on /items, send JSON in the request body, and return the response callback
+  function createItem(name, callback) {
+    const newItem = JSON.stringify({
+      name: name,
+    });
+  }
+
   return {
     getItems,
+    createItem,
   };
 }());
